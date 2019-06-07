4c40ceadcc0580ab99e340cbdc7da7c307e058df
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-07 16:29:54 -0500

    added basic function structure to js

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+//data source should reflect each meal with as an object with meal price, tax rate, tip percentage, subtotal, total
+
+//data source should reflect meal count and total tips as well as average tips
+
+//one form that accepts meal price, tax percentage, and tip
+
+//button that allows to cancel without eliminating all data
+
+//
+function generateMealElement(item) {
+
+}
+
+function renderWaitStaffCalculator() {
+    
+}
+
+
+function handleSubmit() {
+    //listens for submit
+    //saves meal
+    //renders mealdata
+    //renders totaltips
+}
+
+function handleReset() {
+
+}
+
+function handleCancel() {
+
+}
+
+function handleWaitstaffCalculator() {
+    handleSubmit();
+    handleReset();
+    handleCancel();
+}
+
+$(handleWaitstaffCalculator);
